7da06590a44dce850dece842686d717eb2b7acbd
Shridhar Manvi <shridharmanvi@gmail.com>
2018-04-30 12:13:58 -0400

    Adding travis yaml file

---
+++ src/test/java/com/example/mediastreaming/TestSample.java
@@ -0,0 +1,15 @@
+package com.example.mediastreaming;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestSample {
+
+    @Test
+    public void testSample() {
+        int x = 10;
+        int y = 10;
+        assertEquals(x, y);
+    }
+}
